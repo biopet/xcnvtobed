bbf668c288dd0cf5df49955d8be447df4e1d3776
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:26 +0200

    Added template to all tools

---
+++ src/main/scala/nl/biopet/tools/xcnvtobed/Args.scala
@@ -0,0 +1,5 @@
+package nl.biopet.tools.xcnvtobed
+
+import java.io.File
+
+case class Args(inputFile: File = null)
---
+++ src/test/scala/nl/biopet/tools/xcnvtobed/XcnvToBedTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.xcnvtobed
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object XcnvToBedTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
