b530e04e12f2c1736cbdbe6cadc84bacd052ceae
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-14 11:04:22 +0200

    Added code XcnvToBed

--- src/main/scala/nl/biopet/tools/xcnvtobed/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/xcnvtobed/ArgsParser.scala
@@ -5,10 +5,24 @@ import java.io.File
 import nl.biopet.utils.tool.AbstractOptParser
 
 class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
-  opt[File]("inputFile")
-    .abbr("i")
-    .unbounded()
-    .required()
-    .maxOccurs(1)
-    .action((x, c) => c.copy(inputFile = x))
+  head(
+    "Convert a sample track within an XHMM XCNV file to a BED track. Fourt column indicates deletion (-1), normal (0) or duplication (1) of region")
+
+  opt[File]('I', "Input") required () valueName "<xcnv>" action { (x, c) =>
+    c.copy(inputXcnv = x)
+  } validate { x =>
+    if (x.exists) success else failure("Input XCNV not found")
+  } text {
+    "Input XCNV file"
+  }
+  opt[File]('O', "Output") required () valueName "<bed>" action { (x, c) =>
+    c.copy(outputBed = x)
+  } text {
+    "Output BED file"
+  }
+  opt[String]('S', "Sample") required () action { (x, c) =>
+    c.copy(sample = x)
+  } text {
+    "The sample which to select"
+  }
 }
--- src/main/scala/nl/biopet/tools/xcnvtobed/XcnvToBed.scala
+++ src/main/scala/nl/biopet/tools/xcnvtobed/XcnvToBed.scala
@@ -1,6 +1,10 @@
 package nl.biopet.tools.xcnvtobed
 
+import java.io.PrintWriter
+
 import nl.biopet.utils.tool.ToolCommand
+
+import scala.io.Source
 
 object XcnvToBed extends ToolCommand {
   def main(args: Array[String]): Unit = {
@@ -10,7 +14,17 @@ object XcnvToBed extends ToolCommand {
 
     logger.info("Start")
 
-    //TODO: Execute code
+    val writer = new PrintWriter(cmdArgs.outputBed)
+    Source
+      .fromFile(cmdArgs.inputXcnv)
+      .getLines()
+      .filter(!_.startsWith("SAMPLE"))
+      .map(x => x.split("\t"))
+      .map(x => XcnvBedLine(x(0), x(1), x(2)))
+      .filter(_.sample == cmdArgs.sample)
+      .foreach(x => writer.println(x.toString))
+
+    writer.close()
 
     logger.info("Done")
   }
